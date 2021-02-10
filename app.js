7effc3b3cd8ddcb55ce3f7f443f2034fd6c76a1d
chamberezigbo <chamberezigbo@gmail.com>
2021-02-10 10:55:57 +0100

    error debug

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 "use script"
 
-let creditUnit = 25;
+let creditUnit = 25 ;
 
-let grade = creditUnit >= 80 && creditUnit == 100 ? "A" : creditUnit >= 75 ? "B" :
-   creditUnit >= 60 ? "C" : creditUnit >= 45 ? "D" : creditUnit >= 30 ? "E" : 
+let grade = creditUnit >= 80 && creditUnit == 100 ? "A" : creditUnit >= 75 && creditUnit < 80  ? "B" :
+   creditUnit >= 60 && creditUnit < 75 ? "C" : creditUnit >= 45 && creditUnit < 60 ? "D" :
+   creditUnit >= 30 && creditUnit < 45 ? "E" : 
    creditUnit >= 0 ? "f" : "Not valid" ;
 
 console.log(grade)
